73785a16df0b54160fdcdb78904ab71115075fb6
shruthan650 <shruthan41@gmail.com>
2026-08-17 17:10:12 +0530

    Added for-loop into Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,9 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+
+        for (int i = 0; i < 100; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
