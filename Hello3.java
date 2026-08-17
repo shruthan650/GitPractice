bef66d505189c2e0369a4b386cb8a950b60a0b47
shruthan650 <shruthan41@gmail.com>
2026-08-17 17:19:52 +0530

    Added do while loop in Hello3.java

---
+++ Hello3.java
@@ -0,0 +1,10 @@
+public class Hello3 {
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        do {
+            System.out.print(i + " ");
+        } while (i < 1000);
+    }
+}
