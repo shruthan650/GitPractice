0873e8b23da88e42fe58597c2c00f5cc9ac43205
shruthan650 <shruthan41@gmail.com>
2026-08-17 17:13:38 +0530

    Added while loop to Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,10 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+        int i = 0;
+
+        while (i < 100) {
+            System.out.print(i + " ");
+        }
     }
 }
